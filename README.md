42602a4fa96f825dcaede66d1585fb311f94314f
perajarac <petarmijatovic99@gmail.com>
2025-01-17 12:05:23 +0100

    Added simple bash script for building app

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ Clone repo:
 git clone https://github.com/perajarac/simple_chat_app.git
 ```
 
-Run backend:
+Run simple-chat-app locally:
+
+    There is variable NUM_TABS in bash script which can be modified to run as much instances as you want
 
 ```bash
-cd backend
-go run *.go
+chmod +x start.sh
+./start.sh
 ```
---
+++ start.sh
@@ -0,0 +1,23 @@
+
+# Set the number of browser tabs to open
+NUM_TABS=4  # Change this value as needed
+
+cd backend || { echo "Backend folder not found!"; exit 1; }
+
+# Run Go application in the background
+echo "Starting backend..."
+go run *.go &
+
+BACKEND_PID=$!
+
+# Wait a few seconds to let the server start
+sleep 3
+
+# Open the URL in multiple browser tabs
+echo "Opening $NUM_TABS browser instances..."
+for ((i = 0; i < NUM_TABS; i++)); do
+    xdg-open http://127.0.0.1:8080/inital.html &
+done
+
+# Wait for backend process to finish
+wait $BACKEND_PID
